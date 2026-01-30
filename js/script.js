cc1b08e3a2c950a3ea8347aa62adde623e345c06
Musa6571 <mussaabbasi1008@gmail.com>
2026-01-30 10:12:39 +0000

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Basic script file - will add functionality later
+console.log("Web Dev Recruitment Platform loaded");
+
+// Simple navigation toggle for mobile (to be implemented)
+function toggleMenu() {
+    console.log("Menu toggle would work here");
+}
